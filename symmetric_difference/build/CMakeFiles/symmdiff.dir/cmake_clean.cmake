67c1433e1cca5c1e0f4e225fe9c4b1b641051da4
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-02-13 23:58:41 +0300

    rearranged files

---
+++ symmetric_difference/build/CMakeFiles/symmdiff.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/symmdiff.dir/symmdiff.cpp.o"
+  "CMakeFiles/symmdiff.dir/symmdiff.cpp.o.d"
+  "symmdiff"
+  "symmdiff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/symmdiff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
