e758d4f6ca8a3c2f10520b179957f6bb1420c5fb
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-02-14 21:21:44 +0300

    Horner's scheme implemeted

--- polynomial_calculation/polycalc.cpp
+++ polynomial_calculation/polycalc.cpp
@@ -20,6 +20,15 @@ Run_options read_run_options(std::basic_istream<CharT>& is);
 template<typename OutputIt, typename CharT = char, typename IntType = int>
 void read_sequence(std::basic_istream<CharT>& is, OutputIt dest, int num);
 
+template<typename IntType = int>
+int polycalc_horner(const std::vector<IntType>& coeffs, IntType val);
+
+template<typename IntType = int>
+int polycalc_horner_stabilized(const std::vector<IntType>& coeffs, IntType val);
+
+template<typename Type>
+Type fast_pow(Type value, int degree);
+
 int main() {
 
   std::vector<int> coeffs, values;
@@ -33,6 +42,16 @@ int main() {
   read_sequence(std::cin, std::back_inserter(values), run_opts.val_num);
 
   std::cin.exceptions(prev_exc_mask);
+
+  for (auto val : values) {
+
+    #if !defined(STAB)
+      std::cout << polycalc_horner(coeffs, val) << std::endl;
+    #else 
+      std::cout << polycalc_horner_stabilized(coeffs, val) << std::endl;
+    #endif
+  }
+
   return 0;
 }
 
@@ -59,4 +78,51 @@ void read_sequence(std::basic_istream<CharT>& is, OutputIt dest, int num) {
   while (count++ < num && is >> elem) {
     *dest++ = elem;
   }
+}
+
+template<typename IntType>
+int polycalc_horner(const std::vector<IntType>& coeffs, IntType val) {
+
+  IntType res = coeffs.back();
+  auto size = coeffs.size();
+
+  for (int ind = size - 2; ind >= 0; --ind) {
+    res = res * val + coeffs[ind];
+  }
+
+  return res;
+}
+
+template<typename IntType>
+int polycalc_horner_stabilized(const std::vector<IntType>& coeffs, IntType val) {
+
+  auto size = coeffs.size();  
+
+  if (std::abs(val) < 1) {
+    return polycalc_horner(coeffs, val);
+  }
+
+  float res = (float) coeffs.front();
+  float val_inv = 1. / float(val);
+
+  for (int ind = 1; ind < size; ++ind) {
+    res = res * val_inv + float(coeffs[ind]);
+  }
+
+  return fast_pow(val, size - 1) * res;
+}
+
+template<typename Type>
+Type fast_pow(Type value, int degree) {
+
+  if (degree == 0) {
+    return Type(1);
+  }
+
+  if (degree & 1) {
+    return fast_pow(value, degree - 1);
+  }
+
+  Type med = fast_pow(value, degree >> 2);
+  return med * med;
 }
