eab1229231b0992a4ad2e58c0fbba2b6bd4b5cb1
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-02-26 16:13:03 +0300

    add addition operations

--- libs/lint/inc/lint.hpp
+++ libs/lint/inc/lint.hpp
@@ -46,15 +46,11 @@ public:
   Lint& operator+=(const Lint& that);
   Lint& operator-=(const Lint& that);
 
-  Lint& add_digits(const std::deque<char>& that);
-  Lint& sub_digits(const std::deque<char>& that);
-
   std::deque<char> get_digits() const {
     return digits_;
   }
 
-  template<typename CharT>
-  operator std::basic_string<CharT>();
+  operator std::string() const;
 
   using size_type = std::size_t;
 
@@ -73,84 +69,22 @@ public:
   bool is_neg() const {
     return is_neg_;
   }
+
+private:
+
+  void swap_sign() {
+    is_neg_ = !is_neg_;
+  }
+
+  void add_digits(const std::deque<char>& that_digits, size_type that_length);
+  void sub_digits(const std::deque<char>& that_digits, size_type that_length);
 };
-
-Lint::Lint(const std::string& str) {
-
-  std::istringstream isstream(str);
-
-  auto prev_exc_mask = isstream.exceptions();
-  isstream.exceptions(std::ios_base::badbit);
-
-  char ch;
-  isstream >> ch;
-
-  if (isstream.eof()) {
-    return; // EOF - empty
-
-  } else if (ch == '-' || ch == '+') {
-    is_neg_ = (ch == '-');
-
-  } else if (std::isdigit(ch)) {
-    isstream.unget();
-
-  } else {
-
-    isstream.exceptions(prev_exc_mask);
-    throw std::invalid_argument("Invalid string in LONGARITHM::Lint ctor: " + str);
-  }
-
-  while (isstream.get(ch)) {
-    digits_.push_front(ch);
-  }
-
-  isstream.exceptions(prev_exc_mask);
-
-  if (!isstream.eof()) {
-    throw std::invalid_argument("Invalid string in LONGARITHM::Lint ctor: " + str);
-  }
-}
-
-template<typename CharT>
-Lint::operator std::basic_string<CharT>() {
-
-  std::stringstream buf;
-
-  if (length() == 0) {
-
-    buf << '0';
-    return buf.str();
-  }
-
-  if (is_neg_) {
-    buf << '-';
-  }
-
-  std::copy(std::rbegin(digits_), std::rend(digits_), std::ostream_iterator<char>(buf));
-  return buf.str();
-}
-
-template<typename CharT>
-std::basic_istream<CharT>& operator>>(std::basic_istream<CharT>& is, Lint& lint) {
-
-  std::basic_string<CharT> str;
-  std::getline(is, str);
-
-  std::cout << "string read: " << str << std::endl;
-
-  lint = str;
-  return is;
-}
-
-template<typename CharT>
-std::basic_ostream<CharT>& operator<<(std::basic_ostream<CharT>& os, Lint& lint) {
-
-  os << std::basic_string<CharT>(lint);
-  return os;
-}
 
 Lint operator+(const Lint& lhs, const Lint& rhs);
 Lint operator-(const Lint& lhs, const Lint& rhs);
+
+std::istream& operator>>(std::istream& is, Lint& lint);
+std::ostream& operator<<(std::ostream& os, const Lint& lint);
 
 }; // namespace LONGARITHM
 
--- libs/lint/src/lint.cpp
+++ libs/lint/src/lint.cpp
@@ -6,29 +6,68 @@ namespace LONGARITHM {
 
 Lint& Lint::operator+=(const Lint& that) {
 
-  auto max_len = std::max(length(), that.length());
-  auto min_len = std::min(length(), that.length());
-
-  bool carrier = false;
-  for (int ind = 0; ind < min_len; ++ind) {
-
-
+  
+  if ((!is_neg_ && that.is_neg_) || (is_neg_ && !that.is_neg_)) {
+    sub_digits(that.digits_, that.length());
   }
 
+  add_digits(that.digits_, that.length());
   return *this;
 }
 
 Lint& Lint::operator-=(const Lint& that) {
 
+  if ((!is_neg_ && that.is_neg_) || (is_neg_ && !that.is_neg_)) {
+    add_digits(that.digits_, that.length());
+  }
+
+  sub_digits(that.digits_, that.length());
   return *this;
 }
 
-Lint& Lint::add_digits(const std::deque<char>& that) {
+void Lint::add_digits(const std::deque<char>& that_digits, size_type that_length) {
 
+  int max_len, min_len;
+  bool this_longer = (length() > that_length);
+
+  if (this_longer) {
+
+    max_len = length();
+    min_len = that_length;
+
+  } else {
+
+    max_len = that_length;
+    min_len = length();
+  }
+
+  bool carrier = false;
+  for (int ind = 0; ind < min_len; ++ind) {
+
+    char digit = digits_[ind] + that_digits[ind] + carrier;
+
+    digits_[ind] = digit % 10;
+    carrier = digit / 10;
+  }
+
+  if (!this_longer) {
+
+    if (!(length() < that_length) && carrier) {
+      digits_.push_back(1);
+
+    } else {
+
+      for (int ind = min_len; ind < max_len; ++ind) {
+        digits_.push_back(that_digits[ind]);
+      }
+
+      digits_[min_len] += carrier;
+    }
+  }
 }
 
-Lint& Lint::sub_digits(const std::deque<char>& that) {
-  
+void Lint::sub_digits(const std::deque<char>& that_digits, size_type that_length) {
+
 }
 
 Lint operator+(const Lint& lhs, const Lint& rhs) {
@@ -43,6 +82,77 @@ Lint operator-(const Lint& lhs, const Lint& rhs) {
   auto temp{lhs};
   temp -= rhs;
   return temp;
+}
+
+std::ostream& operator<<(std::ostream& os, const Lint& lint) {
+
+  os << std::string(lint);
+  return os;
+}
+
+std::istream& operator>>(std::istream& is, Lint& lint) {
+
+  std::string str;
+  std::getline(is, str);
+
+  std::cout << "string read: " << str << std::endl;
+
+  lint = str;
+  return is;
+}
+
+Lint::Lint(const std::string& str) {
+
+  std::istringstream isstream(str);
+
+  auto prev_exc_mask = isstream.exceptions();
+  isstream.exceptions(std::ios_base::badbit);
+
+  char ch;
+  isstream >> ch;
+
+  if (isstream.eof()) {
+    return; // EOF - empty
+
+  } else if (ch == '-' || ch == '+') {
+    is_neg_ = (ch == '-');
+
+  } else if (std::isdigit(ch)) {
+    isstream.unget();
+
+  } else {
+
+    isstream.exceptions(prev_exc_mask);
+    throw std::invalid_argument("Invalid string in LONGARITHM::Lint ctor: " + str);
+  }
+
+  while (isstream.get(ch)) {
+    digits_.push_front(ch);
+  }
+
+  isstream.exceptions(prev_exc_mask);
+
+  if (!isstream.eof()) {
+    throw std::invalid_argument("Invalid string in LONGARITHM::Lint ctor: " + str);
+  }
+}
+
+Lint::operator std::string() const {
+
+  std::stringstream buf;
+
+  if (length() == 0) {
+
+    buf << '0';
+    return buf.str();
+  }
+
+  if (is_neg_) {
+    buf << '-';
+  }
+
+  std::copy(std::rbegin(digits_), std::rend(digits_), std::ostream_iterator<char>(buf));
+  return buf.str();
 }
 
 }; // namespace LONGARITHM
