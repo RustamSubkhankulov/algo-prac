cc0e58888b9a60cb1aaf00fc17186a6d856703dc
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-03-30 01:54:01 +0300

    16th task done

--- 16wires/src/task.cpp
+++ 16wires/src/task.cpp
@@ -9,9 +9,42 @@ int main() {
   std::cin >> num >> k;
 
   std::vector<int> wires;
-  std::copy_n(std::istream_iterator<int>(std::cin), num, std::back_inserter(wires));
+
+  int sum = 0;
+  for (int n = 0; n < num; ++n) {
+
+    int temp;
+    std::cin >> temp;
+
+    sum += temp;
+    wires.push_back(temp);
+  }
 
   std::sort(std::begin(wires), std::end(wires), std::greater{});
 
-  
+  int m = 0, right = sum, left = 0;
+  int med = ((right - left) >> 1) + left;
+
+  while (med != left) {
+
+    int count = 0;
+    for (int ind = 0; ind < num && wires[ind] >= med; ++ind) {
+      count += wires[ind]/med;
+    }
+
+    if (count >= k) {
+
+      m = (med > m)? med : m;
+      left = med;
+
+    } else {
+
+      right = med;
+    }
+
+    med = ((right - left) >> 1) + left;
+  }
+
+  std::cout << m;
+  return 0;
 }
